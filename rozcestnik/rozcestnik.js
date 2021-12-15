1480944f3522b33ec79476e002c0739da2d2cf45
az-usual <47920627+notsousual@users.noreply.github.com>
2021-12-15 15:55:07 +0100

    rozcestnik add

---
+++ rozcestnik/rozcestnik.js
@@ -0,0 +1,29 @@
+// let bubbleElement = document.getElementById('test')
+// let answerButtonsElement = document.getElementById('bubbles_container')
+
+// const requestAnimationFrame = window.requestAnimationFrame
+
+
+
+// function randomColor() {
+//     return 'rgb(' + getRandomInt(20, 256) + ',' + getRandomInt(20, 256) + ',' + getRandomInt(20, 256)+ ')'
+
+// }
+
+// function getRandomInt(min, max) {
+//     min = Math.ceil(min);
+//     max = Math.floor(max);
+//     return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+//   }
+
+// setInterval(myTimer, 5000);
+
+
+
+// function myTimer() {
+//   console.log(randomColor())
+//   Array.from(answerButtonsElement.children).forEach(button => {
+//     button.style.backgroundColor = randomColor();
+//   })
+// }
+// myTimer();
