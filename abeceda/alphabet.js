c0294749db8543bb67d294571a9358897d33d1e3
az-usual <47920627+notsousual@users.noreply.github.com>
2021-12-30 17:15:18 +0100

    second commit

--- abeceda/alphabet.js
+++ abeceda/alphabet.js
@@ -4,11 +4,23 @@ let a = document.getElementById('body')
 let alphabet = document.getElementById('alphabet')
 let cover = document.getElementById('cover')
 
-let nameEl = document.getElementById("name_input")
-nameEl.addEventListener('click', hello)
+let submitEl = document.querySelector('.submit-btn')
+submitEl.addEventListener('click', start)
 
+let restartEl = document.querySelector('.restart-btn')
+restartEl.addEventListener('click', restart)
+let wordArray;
+
+let nameEl = document.getElementById("name_input")
+nameEl.addEventListener("keyup", function(event) {
+    if (event.key === 'Enter') {
+     event.preventDefault();
+     submitEl.click();
+    }
+});
 
 let count = 7
+let counter = 7
 let colors = [
     '#F15353', // red
     '#F17553', // orange
@@ -21,52 +33,133 @@ let colors = [
     'rgb(202, 111, 146)', //pink
 ]
 
-let content = {
-    a_letter : {text:'Amber', img:'blur1.jpeg'},
-    
-    
-    b_letter : {text: "Beetroot, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. <br><br> Beetroot, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown pr", img:'blur3.jpeg'},    
-    
-    
-    c_letter : {text: "Calculus. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ", img:''},   
-    
-    
-    d_letter : {text: 'Dedication', img:''},  
-    
-    
-    e_letter : {text: 'Epsilon',  img:''},  
-    
-    
-    f_letter : {text: 'Figure',   img:''}, 
-    
-    
-    g_letter : {text: 'Goose',    img:''},
-    
-    
-    h_letter : {text: 'Hectic',   img:''}
-    
+let diacritics = {
+    'ě': 'e_letter',
+    'š': 's_soft_letter',
+    'č': 'c_soft_letter',
+    'ř': 'r_soft_letter',
+    'ž': 'z_soft_letter',
+    'ý': 'y_letter',
+    'á': 'a_letter',
+    'í': 'i_letter',
+    'é': 'e_letter',
+    'ú': 'u_letter',
+    'ů': 'u_letter'
 }
 
-function hello(){
-    var name = document.getElementById("name_input").value;
-    console.log(name)
-  }
+let content = {
+    a_letter : {text:'Amber', img:'blur1.jpeg'},
+    b_letter : {text: "Beetroot, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. <br><br> Beetroot, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown pr", img:'blur3.jpeg'},    
+    c_letter : {text: "Calculus. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ", img:''},   
+    d_letter : {text: 'Dedication', img:''},  
+    e_letter : {text: 'Epsilon',  img:''},  
+    f_letter : {text: 'Figure',   img:''}, 
+    g_letter : {text: 'Goose',    img:''},
+    h_letter : {text: 'Hectic',   img:''},
+    z_soft_letter : {text: 'Žába',   img:''}
+    
+}
+console.log(diacritics)
+
+function start(){
+    restart()
+    let name = nameEl.value;
+    let wordArray = name.split("");
+    
+    for (let i=0; i<wordArray.length; i++) {
+        console.log(wordArray[i])
+        if (wordArray[i] === 'ň') {
+            wordArray[i] = 'n_soft_letter'
+        
+        } else if ( wordArray[i] === 'ě') {
+            wordArray[i] = 'e_letter'
+
+        } else if (wordArray[i] === 'š') {
+            wordArray[i] = 's_soft_letter'
+
+        } else if (wordArray[i] === 'č') {
+            wordArray[i] = 'c_soft_letter'
+
+        } else if (wordArray[i] === 'ř') {
+            wordArray[i] = 'r_soft_letter'
+
+        } else if (wordArray[i] === 'ž') {
+            wordArray[i] = 'z_soft_letter'
+
+        } else if (wordArray[i] === 'ť') {
+            wordArray[i] = 't_soft_letter'
+
+        } else if (wordArray[i] === 'ď') {
+            wordArray[i] = 'd_soft_letter'
+
+        } else if (wordArray[i] === 'ý') {
+            wordArray[i] = 'y_letter'
+
+        } else if (wordArray[i] === 'á') {
+            wordArray[i] = 'a_letter'
+
+        } else if (wordArray[i] === 'í') {
+            wordArray[i] = 'i_letter'
+
+        } else if (wordArray[i] === 'é') {
+            wordArray[i] = 'e_letter'
+
+        } else if (wordArray[i] === 'ú') {
+            wordArray[i] = 'u_letter'
+
+        } else if (wordArray[i] === 'ů') {
+            wordArray[i] = 'u_letter'
+
+        } else  {
+            wordArray[i] += '_letter'
+        }
+    }
+    // console.log(name)
+    console.log(wordArray)
+    
+    Array.from(alphabet.children).forEach( card => {
+
+        card.style.backgroundColor = colors[count % 7]
+        card.dataset.ourcolor = colors[count % 7]
+        Array.from(card.children).forEach ( child => {
+            child.dataset.ourcolor = colors[count % 7]
+        })
+        count += 1
+
+        if (!(wordArray.includes(card.id))) {
+            card.style.display = 'none'
+        }
+
+        }
+    )
+}
+
+function restart() {
+
+    Array.from(alphabet.children).forEach( card => {
+        card.style.display = 'flex'
+        })
+
+}
+
+
+
+
 
 function show(e) {
     
-
     let our_color = window.getComputedStyle(e.target).getPropertyValue("background-color");
     our_color = e.target.dataset.ourcolor
     console.log(e.target)
     if (!(e.target.dataset.ourcolor)) {
         window.getComputedStyle(e.target.parentElement).getPropertyValue("background-color");
     }
-    console.log(our_color)
 
-    console.log('show', e.target)
+    // console.log(our_color)
+    // console.log('show', e.target)
+
     cover.style.backgroundColor = our_color
     cover.style.display = 'flex'
-    // document.getElementById(e.target.id).style.display = 'none'
 
     if (content[e.target.dataset.id].img) {
         cover.innerHTML = `<img src="${content[e.target.dataset.id].img}"> <h3>${content[e.target.dataset.id].text}</h3>`
@@ -74,16 +167,12 @@ function show(e) {
     } else {
         cover.innerHTML = `<h3>${content[e.target.dataset.id].text}</h3>`
     }
-    
-        
-    // e.target.style.backgroundColor = 'white'
+
 }
 
 function unshow(e) {
-    console.log('Done')
+    // console.log('Done')
     cover.style.display = 'none'
- 
-
 }
 
 function compoundWord() {
@@ -91,28 +180,21 @@ function compoundWord() {
 }
 
 
-
 function attachColors() {
-
-
-    
     
     Array.from(alphabet.children).forEach( card => {
         card.style.backgroundColor = colors[count % 7]
-
         card.dataset.ourcolor = colors[count % 7]
-  
+
         Array.from(card.children).forEach ( child => {
             child.dataset.ourcolor = colors[count % 7]
         })
-        console.log(count, count % 7 - 1)
+        // console.log(count, count % 7 - 1)
         count += 1
 
         card.addEventListener('click', show)
-
         }
     )
-
 }
 
 
