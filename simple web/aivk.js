a4c60050cda2be7d642fc6421ef2587237d75317
Trinithea <mysakmazplus@gmail.com>
2022-01-21 15:46:22 +0100

    frantisek: zmena popisku

--- simple web/aivk.js
+++ simple web/aivk.js
@@ -18,7 +18,7 @@ let people = {
   'jindrich': 'Informatika, AI v kontextu přirozeného jazyka',
   'kajzar': 'Lékař, AI v medicíně',
   'nikol': 'Psycholožka, AI v psychologii',
-  'frantisek': 'Teolog, AI v teologii'
+  'frantisek': 'Teolog, AI v teologii a náboženství'
 }
 
 let description = document.querySelector('.description')
