0ae9287dbc8bc721904880268c75f769b25b46d3
Trinithea <mysakmazplus@gmail.com>
2022-03-07 18:26:58 +0100

    ai v med i s menu

--- exponat/menu/script_menu.js
+++ exponat/menu/script_menu.js
@@ -1,6 +1,7 @@
 let menubtn = document.getElementById("menu-btn")
 let menuitems = document.getElementById("menu-items")
 
+
 function showMenu(){
     if (menuitems.style.display == "none") {
         menuitems.style.display = "flex";
