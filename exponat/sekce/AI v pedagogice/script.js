2973c1d7d87ae30841949dd8ef8f91d44e37151f
Trinithea <mysakmazplus@gmail.com>
2022-03-07 17:00:38 +0100

    AI v pedagogice

---
+++ exponat/sekce/AI v pedagogice/script.js
@@ -0,0 +1,20 @@
+let firstSection = document.getElementById('first-section')
+let secondSection = document.getElementById('second-section')
+let firstSection2 = document.getElementById('first-section2')
+let secondSection2 = document.getElementById('second-section2')
+
+
+
+function show() {
+    firstSection.classList.add('hide')
+    secondSection.classList.remove('hide')
+}
+function show2() {
+    firstSection2.classList.add('hide')
+    secondSection2.classList.remove('hide')
+}
+
+
+function submit() {
+
+}
