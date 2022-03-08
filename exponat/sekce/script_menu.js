eb72784dc1e1436423988c722ad778c40f4fe600
Trinithea <mysakmazplus@gmail.com>
2022-03-08 22:04:26 +0100

    menu odkazy, kořenový systém

--- exponat/sekce/script_menu.js
+++ exponat/sekce/script_menu.js
@@ -1,17 +1,11 @@
-let menubtn = document.getElementById("menu-btn")
+
 let menuitems = document.getElementById("menu-items")
 
 
 function showMenu(){
     if (menuitems.style.display == "none") {
         menuitems.style.display = "flex";
-        document.getElementById("next-button").style.display="none";
-        document.getElementById("prev-button").style.display="none";
-        
       } else {
         menuitems.style.display = "none";
-        document.getElementById("next-button").style.display="flex";
-        document.getElementById("prev-button").style.display="flex";
-        
       }
 }
