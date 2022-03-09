33a6e95bc5a7878e6a9efffbd36484d5a8d19141
alice z <alisazakharenko@gmail.com>
2022-03-09 07:59:40 +0100

    fixing menu

--- exponat/sekce/script_menu.js
+++ exponat/sekce/script_menu.js
@@ -1,11 +1,36 @@
 
+// let menuitems = document.getElementById("menu-items")
+
+
+// function showMenu(){
+//     if (menuitems.style.display == "none") {
+//         menuitems.style.display = "fixed";
+//       } else {
+//         menuitems.style.display = "none";
+//       }
+// }
+
+let menubtn = document.getElementById("menu-btn")
 let menuitems = document.getElementById("menu-items")
+// let bar = document.getElementById("bar")
+var x = window.matchMedia("(max-width: 500px)")
+
+let display = 'fixed'
+
 
 
 function showMenu(){
     if (menuitems.style.display == "none") {
-        menuitems.style.display = "flex";
+        menuitems.style.display = display;
+
       } else {
         menuitems.style.display = "none";
+        if(x.matches){
+  
+        }
+        else {
+ 
+        }
+        
       }
 }
