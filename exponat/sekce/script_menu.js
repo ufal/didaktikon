bac6655d3ef86d8c3cae5037ab7cfc8803c41007
Trinithea <mysakmazplus@gmail.com>
2022-03-14 21:04:01 +0100

    položky v menu jako JS objekt (rozdělané)

--- exponat/sekce/script_menu.js
+++ exponat/sekce/script_menu.js
@@ -2,12 +2,26 @@
 // let menuitems = document.getElementById("menu-items"
 let menubtn = document.getElementById("menu-btn")
 let menuitems = document.getElementById("menu-items")
+let hrefs = document.getElementsByClassName("menu-href")
 
 let display = 'block'
 
+let items = {
+  deti : {href:"../AI pro deti/aiprodeti.html",text:"AI očima dětí"},
+  medicina : {href:"../AI v medicine/index.html",text:"AI v medicíně"},
+  pedagogika : {href:"../AI v pedagogice/first.html",text:"AI v pedagogice"},
+  nabozenstvi : {href:"../nabozenstvi/first.html",text:"AI v náboženství"},
+  neuroscince : {href:"",text:"AI v neurovědě"},
+  divadlo : {href:"",text:"AI v kontextu divadla"},
+  fyzika : {href:"",text:""}
+}
 
 
 function showMenu(){
+  for (let i = 0; i < hrefs.length; i++) { 
+    hrefs[i].href = items[i].href;
+    hrefs[i].innerHTML = items[i].text;
+  }
     if (menuitems.classList.contains('hide')) {
       menuitems.classList.remove('hide')
       menuitems.classList.add('show-menu')
@@ -17,3 +31,4 @@ function showMenu(){
         menuitems.classList.add('hide')
       }
 }
+
