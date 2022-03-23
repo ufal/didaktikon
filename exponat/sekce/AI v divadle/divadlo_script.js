5e6f44a325b38ce756dd87fe8fd5f4cfb174b34e
Trinithea <mysakmazplus@gmail.com>
2022-03-23 09:42:47 +0100

    AI v divadle hotovo až na robota

---
+++ exponat/sekce/AI v divadle/divadlo_script.js
@@ -0,0 +1,14 @@
+let vyhodnoceni = document.getElementById("vyhodnoceni")
+
+function check(vystup){
+    if(vystup=="A"){ /*správně*/
+        vyhodnoceni.innerHTML="Výborně :)"
+        vyhodnoceni.style.color = "rgb(161, 211, 100)"
+
+    }
+    else if(vystup=="B"){
+        vyhodnoceni.innerHTML="Bohužel :("
+        vyhodnoceni.style.color="rgb(230, 101, 101)"
+    }
+    document.getElementById("pie-chart").style.display="block"
+}
