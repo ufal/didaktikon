a0f92de214ea4cba2328976442d026704a42cccb
Trinithea <mysakmazplus@gmail.com>
2022-04-12 11:02:40 +0200

    db pokus2

--- simple web/db.php
+++ simple web/db.php
@@ -11,6 +11,9 @@ $conn = new mysqli($server, $username, $password, $database);
 if($conn->connect_error){
     die("Chyba připojení: ".$conn->connect_error);
 }
+else{
+    echo "<p>Úspěšně připojeno</p>"
+}
 $sql="INSERT INTO Fyzika VALUES ('pokus1', 'pokus2');";
 $conn->query($sql);
 ?>
