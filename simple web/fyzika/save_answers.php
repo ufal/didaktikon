3d31b8cc4df021d97cc331f2e4976549a1d2a2c9
Trinithea <mysakmazplus@gmail.com>
2022-04-12 10:47:00 +0200

    save answers

--- simple web/fyzika/save_answers.php
+++ simple web/fyzika/save_answers.php
@@ -3,6 +3,9 @@ include "db.php";
 $prvni_otazka=$_POST["prvni-otazka"];
 $druha_otazka=$_POST["druha-otazka"];
 
-$sql="INSERT INTO Fyzika VALUES ($prvni_otazka, $druha_otazka);"
+echo $prvni_otazka;
+echo $druha_otazka;
+
+$sql="INSERT INTO Fyzika VALUES ('$prvni_otazka', '$druha_otazka');"
 $conn->query($sql);
 ?>
