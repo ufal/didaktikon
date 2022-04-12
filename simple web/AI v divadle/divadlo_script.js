925f41e09c98861f3fe37804e2e98f4e0843dda2
Trinithea <mysakmazplus@gmail.com>
2022-04-12 20:58:56 +0200

    zkouška linku

---
+++ simple web/AI v divadle/divadlo_script.js
@@ -0,0 +1,43 @@
+let vyhodnoceni = document.getElementById("vyhodnoceni")
+
+function check(vystup){
+    if(vystup=="A"){ /*správně*/
+        vyhodnoceni.innerHTML="Výborně :)"
+        vyhodnoceni.style.color = "rgb(161, 211, 100)"
+
+    }
+    else if(vystup=="B"){
+        vyhodnoceni.innerHTML="Bohužel :("
+        vyhodnoceni.style.color="rgb(230, 101, 101)"
+    }
+    document.getElementById("pie-chart").style.display="block"
+}
+
+let btns = document.getElementsByClassName("btn")
+btns[0].addEventListener("click", () => {
+ 
+    // Fetching Button value
+    let btnValue = btns[0].value;
+ 
+    // jQuery Ajax Post Request
+    $.post('action.php', {
+        btnValue: btnValue
+    }, (response) => {
+        // response from PHP back-end
+        console.log(response);
+    });
+});
+
+btns[1].addEventListener("click", () => {
+ 
+    // Fetching Button value
+    let btnValue = btns[1].value;
+ 
+    // jQuery Ajax Post Request
+    $.post('action.php', {
+        btnValue: btnValue
+    }, (response) => {
+        // response from PHP back-end
+        console.log(response);
+    });
+});
