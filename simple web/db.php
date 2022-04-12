2014829906c6990f76ad17e0059acf33e45607f8
Trinithea <mysakmazplus@gmail.com>
2022-04-12 12:05:24 +0200

    php.ini prepsano

--- simple web/db.php
+++ simple web/db.php
@@ -2,7 +2,25 @@
   <meta charset="UTF-8">
 </head>
 <body>
-    <?php 
-echo php_ini_loaded_file(); ?>
+<?php
+echo "<p>Zkouška databáze</p>";
+$server = "localhost";
+$database = "aivkDB";
+$username = "mazna";
+$password = "Aivk_Maz7!";
+
+//Create connection
+$conn = new mysqli($server, $username, $password, $database);
+
+//Check connection
+if($conn->connect_error){
+    die("Chyba připojení: ".$conn->connect_error);
+}
+else{
+    echo "<p>Úspěšně připojeno</p>";
+}
+$sql="INSERT INTO Fyzika VALUES (2,'pokus1', 'pokus2');";
+$conn->query($sql);
+?>
 
 </body>
