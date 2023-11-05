e89bab7be3a197f238e90448c74b2b244ce04499
jdosoudil <70068043+jdosoudil@users.noreply.github.com>
2023-11-05 22:05:49 +0100

    Frontend ke generování duše v rámci Náboženství

---
+++ exponat/sekce/nabozenstvi/data/questions-1.js
@@ -0,0 +1,45 @@
+var nabozenstviQuestions1 = [
+    {
+        "question": "Líbí se mi:",
+        "options": ["hory", "moře", "les"],
+        "answers": ["mountains", "sea", "forest"]
+    },
+
+    {
+        "question": "Líbí se mi:",
+        "options": ["van Gogh", "da Vinci", "Picasso", "Frída Kahló"],
+        "answers": ["van Gogh", "da Vinci", "Picasso", "Frída Kahló"],
+        "append": " painting, movie still, beautiful, "
+    },
+
+    {
+        "question": "Co mně v Bibli inspiruje?",
+        "options": ["žalmy", "evangelia", "nic", "Bibli nečtu"],
+        "answers": ["psalms", "gospels", "nothing", "secular"]
+    },
+    
+    {
+        "question": "Který z těchto hříchů je pro Vás nejpřijatelnější?",
+        "options": ["pýcha", "lhostejnost", "marnivost", "lenost"],
+        "answers": ["proud", "indifferent", "vane", "lazy"]
+    },
+
+    {
+        "question": "Nejdůležitější je:",
+        "options": ["víra", "naděje", "láska"],
+        "answers": ["faith", "hope", "love"]
+    },
+
+    {
+        "question": "Duše je:",
+        "options": ["prostorovost", "osoba", "hloubka", "éterická substance"],
+        "answers": ["spatiality", "person", "depth", "etherical substance"]
+    },
+
+    {
+        "question": "Pro milovanou osobu bych neudělal:",
+        "options": ["krádež", "transplantaci", "řízky"],
+        "answers": ["stealing", "transplants", "steaks"],
+        "prepend": "no ",
+    }
+];
---
+++ exponat/sekce/nabozenstvi/data/questions-2.js
@@ -0,0 +1,57 @@
+var nabozenstviQuestions2 = [
+    {
+        "question": "Líbí se mi:",
+        "options": ["město", "vesnice"],
+        "answers": ["city", "village"]
+    },
+
+    {
+        "question": "Líbí se mi:",
+        "options": ["tenis", "fotbal", "hokej", "šachy"],
+        "answers": ["tennis", "football", "ice hockey", "chess"]
+    },
+    
+    {
+        "question": "Líbí se mi:",
+        "options": ["Lada", "Mucha", "Kupka", "Toyen"],
+        "answers": ["Lada", "Mucha", "Kupka", "Toyen"],
+        "prepend": "style of "
+    },
+
+    {
+        "question": "Jaké se Vám líbí oči?",
+        "options": ["hnědé", "modré", "zelené", "černé"],
+        "answers": ["brown", "blue", "green", "black"],
+        "append": " eyes, "
+    },
+    
+    {
+        "question": "Jsou chvíle, kdy je odpustitelné lhát?",
+        "options": ["ne", "ano"],
+        "answers": ["truthful", "liar"]
+    },
+    
+    {
+        "question": "Jsou chvíle, kdy je vhodné dát někomu facku?",
+        "options": ["ano", "ne"],
+        "answers": ["aggressive", "peaceful"]
+    },
+    
+    {
+        "question": "Nejvíce toužím po:",
+        "options": ["penězích", "moci", "slávě"],
+        "answers": ["money", "power", "fame"]
+    },
+    
+    {
+        "question": "Líbí se mi:",
+        "options": ["vzpomínat na minulost", "žít tady a teď", "snít o budoucnosti"],
+        "answers": ["thinking about the past", "living here and now", "dreaming about the future"]
+    },
+    
+    {
+        "question": "Nejdůležitější je:",
+        "options": ["cesta", "cíl", "trasa", "procházka"],
+        "answers": ["journey", "destination", "route", "walk"]
+    }
+];
