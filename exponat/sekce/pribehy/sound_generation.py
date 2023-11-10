692abd3f3b68f3449d44afea79a407d66c5c5392
Rudolf Rosa <rosa@ufal.mff.cuni.cz>
2023-11-10 15:20:24 +0100

    missfiles

---
+++ exponat/sekce/pribehy/sound_generation.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+#coding: utf-8
+
+import sys
+import os
+from image_generation import text2id
+from gtts import gTTS
+
+import logging
+logging.basicConfig(
+    format='%(asctime)s %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    level=logging.INFO)
+
+SNDDIR="gensounds"
+
+def get_audio_for_line(line):
+    filename = f"{SNDDIR}/{text2id(line)}.mp3"
+    try:
+        if not os.path.isfile(filename):
+            tts = gTTS(line, lang='cs', tld='cz', slow=True)
+            tts.save(filename)
+    except Exception as e:
+        message = f'StoryBoarder: Cannot generate sound "{filename}" for "{line}": {e}'
+        logging.warning(message)
+        print(message)
+        filename = f"{SNDDIR}/pokus.mp3"
+    return filename
+
+if __name__=="__main__":
+    line = input('Text:')
+    print(get_audio_for_line(line))
+     
+
+
